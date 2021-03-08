7e98321e3c167877f102ab8eeaa91134aaf1bb41
Niyi Odumosu <niyi.a.odumosu@gmail.com>
2021-03-08 12:43:51 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
 # ccloud-manager
+
+The ccloud-manager allows users to create, describe and delete components in the ccloud clusters. 
+Currently, ccloud-manager supports the management of the following components:
+
+* Connectors
+* ksqlDBs
+* Service Accounts
+* Api-Keys
+
+Ccloud-Manager serves as a python wrapper for the ccloud CLI. 
+This provides a reusable and testable artifact for those who need to create and manage ccloud resources.
+
+It also can be easily integrated with CICD pipelines such as Jenkins, TravisCI and CircleCI. 
+
+When using the ccloud-manager script, one must specify the component (connector, ksqlDB, service-account etc.)
+and the action (create, describe, delete, list and update) as required parameters. 
+
+The optional parameters (which in some cases aren't optional) specify resource locators that identify which resources need to have an action performed on.
+Below are the resource locators parameters for the ccloud components:
+
+--config, The path to the connector config file 
+--cluster,  The id of the cluster
+--description, The description of the service account
+--id,  The id of the ccloud component
+--name, The name of the ksql db app or service account  
+
+## Connector Management
+
+### Create
+In order to create a new connector, one must specify the cluster and the config file with the connector's properties.
+
+`python3 ccloud_component.py connector create --cluster {cluster-id} --config </path/to/connector/config.json>`
+
+### Describe
+In order to describe an existing connector, one must specify the connector id.
+`python3 ccloud_component.py connector describe --id {connector-id}`
+
+### Delete
+In order to delete an existing connector, one must specify the connector id.
+`python3 ccloud_component.py connector delete --id {connector-id}`
+
+### List 
+In order to list all the existing connectors, just add list.
+`python3 ccloud_component.py connector list`
+
+## ksqlDB Management
+
+### Create
+In order to create a new ksqlDB, just give the name of the new ksqlDB app.
+`python3 ccloud_component.py ksqlDB create --name {name}`
+
+### Describe
+In order to describe an existing ksqlDB, one must specify the ksqlDB id.
+`python3 ccloud_component.py ksqlDB describe --id {ksqlDB-id}`
+
+### Delete
+In order to delete an existing ksqlDB, one must specify the ksqlDB id.
+`python3 ccloud_component.py ksqlDB delete --id {ksqlDB-id}`
+
+### List 
+In order to list all the existing ksqlDBs, just add list.
+`python3 ccloud_component.py ksqlDB list`
+
+## Service Account Management
+
+### Create
+In order to create a new service account, just give the name of the new service account app.
+`python3 ccloud_component.py service-account create --name {name}`
+
+### Describe
+In order to describe an existing service account, one must specify the service account id.
+`python3 ccloud_component.py service-account describe --id {service-account-id}`
+
+### Delete
+In order to delete an existing service account, one must specify the service account id.
+`python3 ccloud_component.py service-account delete --id {service-account-id}`
+
+### List 
+In order to list all the existing service accounts, just add list.
+`python3 ccloud_component.py service-account list`
+
+### Update 
+In order to update an existing service account, one must specify the service account id and it's description.
+`python3 ccloud_component.py service-account describe --id {service-account-id} --description {description}`
+
+
+## Api-Keys Management
+
+### Create
+
+### Describe
+
+### Delete
+
+### List 
---
+++ ccloud_component.py
@@ -0,0 +1,225 @@
+
+import click, subprocess
+
+
+def login_to_ccloud():
+    subprocess.run(['ccloud', 'login'])
+
+
+class CCLoudKsqlDB:
+    def create_ksql_app(self, name):
+        """ Create a new ksqldb app
+
+        Args:
+            name: str, the name of the ksqldb app
+        """
+        subprocess.run(['ccloud', 'ksql', 'app', 'create', name])
+
+    def delete_ksql_app(self, id):
+        """ Delete a new ksqldb app
+
+        Args:
+            name: str, the name of the ksqldb app
+        """
+        subprocess.run(['ccloud', 'ksql', 'app', 'delete', id])
+
+    def describe_ksql_app(self, id):
+        """ Describe an exiting ksqldb app
+
+        Args:
+            id: str, the id of the ccloud ksqldb
+        """
+        subprocess.run(['ccloud', 'ksql', 'app', 'describe', id])
+
+    def list_ksql_apps(self,):
+        """ List ksqldb apps"""
+        subprocess.run(['ccloud', 'ksql', 'app', 'list'])
+
+
+class CCLoudConnector:
+
+    def create_connector_app(self, cluster, config):
+        """ Create a new connector app
+
+        Args:
+            cluster: str, the id of the ccloud cluster
+            config: str, the path to the config
+        """
+        subprocess.run(['ccloud', 'connector', 'create', '--cluster', cluster, '--config', config])
+
+    def delete_connector_app(self, id):
+        """ Delete a connector app
+        Args:
+            id: str, the id of the ccloud connector
+        """
+        print("Deleting the connector...")
+        subprocess.run(['ccloud', 'connector',  'delete', id])
+
+    def describe_connector_app(self, id):
+        """ Describe an existing connector app
+
+        Args:
+            id: str, the id of the ccloud connector
+        """
+        subprocess.run(['ccloud', 'connector',  'describe', id])
+
+    def list_connector_apps(self,):
+        """ List the connector apps
+        """
+        subprocess.run(['ccloud', 'connector', 'list'])
+
+
+class CCLoudServiceAccount:
+
+    def create_service_account(self, name):
+        """ Create a new service account
+
+        Args:
+            name: str, the name of the service account
+        """
+        subprocess.run(['ccloud', 'service-account', 'create', name])
+
+    def delete_service_account(self, id):
+        """ Delete a service account
+
+        Args:
+           id: str, the id of the ccloud service account
+        """
+        subprocess.run(['ccloud', 'service-account',  'delete', id])
+
+    def update_service_account(self, id, description):
+        """ Update the description of an existing service account
+
+        Args:
+            id: str, the id of the ccloud service account
+            description: str, the description of the ccloud service account
+        """
+        subprocess.run(['ccloud', 'service-account',  'update', id, description])
+
+    def list_service_accounts(self,):
+        """ List the service accounts
+        """
+        subprocess.run(['ccloud', 'service-account', 'list'])
+
+
+class CCLoudApiKey:
+
+    def create_api_key(self, resource,  service_account, description, output, environment,):
+        """ Create a new api key
+
+        Args:
+            name: str, the name of the api key
+        """
+        subprocess.run(['ccloud', 'api-key', 'create', resource, service_account, description, output, environment])
+
+    def delete_api_key(self, api_key):
+        """ Delete a api key
+
+        Args:
+           api_key: str, the ccloud api_key you wish to delete. It will delete the api secret as well
+        """
+        subprocess.run(['ccloud', 'api-key',  'delete', api_key])
+
+    def update_api_key(self, api_key, description):
+        """ Update the description of an existing api key
+
+        Args:
+            api_key: str, the ccloud api_key you wish to update
+            description: str, the description of the ccloud api key
+        """
+        subprocess.run(['ccloud', 'api-key',  'update', api_key, description])
+
+    def list_api_keys(self,):
+        """ List all the api keys in your environment
+        """
+        subprocess.run(['ccloud', 'api-key', 'list'])
+
+supported_components = {
+    'ksql': {
+        'class': CCLoudKsqlDB(),
+
+    },
+    'connector': {
+        'class': CCLoudConnector(),
+
+    },
+    'service_account': {
+        'class': CCLoudServiceAccount(),
+
+    }
+}
+
+
+supported_actions = (
+    'create',
+    'delete',
+    'describe',
+    'list',
+    'update'
+)
+
+
+@click.command()
+@click.argument('component', type=click.Choice(supported_components.keys()))
+@click.argument('action', type=click.Choice(supported_actions))
+@click.option('--config',  help='The path to the connector config file ')
+@click.option('--cluster',   help='The id of the cluster')
+@click.option('--description',   help='The description of the service account')
+@click.option('--id',   help='The id of the kafka component')
+@click.option('--name',  help='The name of the ksql db app or service account ' )
+def main(component, action, config, cluster, description, id, name):
+    login_to_ccloud()
+    if component == 'connector':
+
+        if action == 'create':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}_app')(cluster, config)
+        elif action == 'describe' or action == 'delete':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}_app')(id)
+        else:
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}_apps')()
+
+    elif component == 'ksql':
+        if action == 'create':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}_app')(name)
+        elif action == 'describe' or action == 'delete':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}_app')(id)
+        else:
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}_apps')()
+
+    elif component =='api-key':
+        if action == 'create':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}')(name)
+        elif action == 'delete':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}')(id)
+        elif action == 'update':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}')(id, description)
+        else:
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}s')()
+
+    else:
+        if action == 'create':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}')(name)
+        elif action == 'delete':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}')(id)
+        elif action == 'update':
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}')(id, description)
+        else:
+            getattr(supported_components[component]['class'],
+                    f'{action}_{component}s')()
+
+
+if __name__ == '__main__':
+    main()
